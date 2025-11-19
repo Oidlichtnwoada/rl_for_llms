7709e05682cec97421ce2408eaddbd0f751ceca4
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2025-11-19 09:03:00 +0100

    feat: try these config values

--- rl_for_llms/models/config.py
+++ rl_for_llms/models/config.py
@@ -17,7 +17,7 @@ class Config(BaseModel):
     learning_rate: float = Field(default=1e-6)
     num_train_epochs: int = Field(default=get_cuda_default_value(3, 1))
     use_vllm: bool = Field(
-        default=get_cuda_default_value(value_if_cuda=False, value_if_not_cuda=False)
+        default=get_cuda_default_value(value_if_cuda=True, value_if_not_cuda=False)
     )
     vllm_gpu_memory_utilization: float = Field(default=0.2)
     vllm_split_model_across_gpus: bool = Field(default=False)
@@ -39,7 +39,7 @@ class Config(BaseModel):
     log_completions: bool = Field(default=True)
     beta: float = Field(default=0.0)
     enable_lora: bool = Field(
-        default=get_cuda_default_value(value_if_cuda=False, value_if_not_cuda=True)
+        default=get_cuda_default_value(value_if_cuda=True, value_if_not_cuda=True)
     )
     lora_target_modules: list[str] = Field(
         default_factory=lambda: ["gate_proj", "up_proj", "down_proj"]
