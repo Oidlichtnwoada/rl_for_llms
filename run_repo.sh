aff4ab715a1b72856205b590519f97afe2cce1a1
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2025-11-19 02:21:26 +0100

    feat: also save output to a log file

--- run_repo.sh
+++ run_repo.sh
@@ -3,5 +3,5 @@
 # make sure the output is unbuffered
 export PYTHONUNBUFFERED=1
 
-# run the project
-uv run -m rl_for_llms.main
+# run the project (output is visible in terminal and appended to a log file)
+uv run -m rl_for_llms.main 2>&1 | tee -a logs/rl_for_llms.main.log
