98689a1ca32c1caf724494892de58c71713b2a17
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2025-11-29 11:51:10 +0100

    feat: changed default value for input size, otherwise eval dataset is empty

--- rl_for_llms/models/config.py
+++ rl_for_llms/models/config.py
@@ -24,7 +24,7 @@ class Config(BaseModel):
     vllm_mode: str = Field(default="colocate")
     report_to: list[str] = Field(default_factory=lambda: ["tensorboard"])
     dataset_use_row_percentage: float = Field(default=get_cuda_default_value(1.0, 0.1))
-    max_prompt_length: int = Field(default=get_cuda_default_value(2048, 32), le=8192)
+    max_prompt_length: int = Field(default=get_cuda_default_value(2048, 64), le=8192)
     max_completion_length: int = Field(
         default=get_cuda_default_value(8192, 1024), le=8192
     )
