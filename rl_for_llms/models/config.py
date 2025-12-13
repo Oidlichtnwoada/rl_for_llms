7d175a031f7c3c38461091bb15bf943a6055df2c
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2025-12-13 23:41:52 +0100

    feat: increased learning rate

--- rl_for_llms/models/config.py
+++ rl_for_llms/models/config.py
@@ -22,7 +22,7 @@ class Config(BaseModel):
     confidence_token_logit_mean: float = Field(default=-4.0)
     confidence_token_logit_std: float = Field(default=1.5)
     confidence_loss_factor: float = Field(default=0.1)
-    learning_rate: float = Field(default=1e-6)
+    learning_rate: float = Field(default=1e-5)
     num_train_epochs: int = Field(default=get_cuda_default_value(3, 1))
     use_vllm: bool = Field(default=False)
     vllm_gpu_memory_utilization: float = Field(default=0.4)
