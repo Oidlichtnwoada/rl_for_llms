f1df43c4c93615fde00cd555319ccb281fb51eaa
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2025-12-14 13:19:36 +0100

    feat: set padding side to left

--- rl_for_llms/utils/llm_utils.py
+++ rl_for_llms/utils/llm_utils.py
@@ -129,11 +129,16 @@ def tokenize_messages(
 
 @cache
 def get_tokenizer(
-    model_id: str, truncation_side: str = "left"
+    model_id: str,
+    truncation_side: str = "left",
+    padding_side: str = "left",
 ) -> PreTrainedTokenizerBase:
     """Return the tokenizer for the specified model ID."""
     tokenizer = AutoTokenizer.from_pretrained(
-        model_id, truncation_side=truncation_side, trust_remote_code=True
+        model_id,
+        truncation_side=truncation_side,
+        padding_side=padding_side,
+        trust_remote_code=True,
     )  # type: ignore[no-untyped-call]
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
