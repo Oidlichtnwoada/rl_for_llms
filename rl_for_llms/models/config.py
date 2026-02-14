e199fde3b355bb5409484d8c8225aa8e8e664b8a
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2026-02-14 12:47:39 +0100

    fix: fixed enabling and disabling of confidence token training

--- rl_for_llms/models/config.py
+++ rl_for_llms/models/config.py
@@ -12,11 +12,11 @@ from rl_for_llms.utils.constant_utils import (
 )
 from rl_for_llms.utils.environment_utils import (
     get_base_num_generations,
+    get_lora_train_confidence_token_embedding,
     get_per_device_rollouts_per_batch,
     get_skip_eval_before_train,
     use_confidence_loss,
     use_confidence_reward,
-    get_lora_train_confidence_token_embedding,
 )
 from rl_for_llms.utils.torch_utils import (
     get_cuda_default_value,
@@ -86,7 +86,9 @@ class Config(BaseModel):
     lora_dropout: float = Field(default=0.05)
     lora_bias: Literal["none", "all", "lora_only"] = Field(default="none")
     lora_task_type: str = Field(default=TaskType.CAUSAL_LM)
-    lora_train_confidence_token_embedding: bool = Field(default=get_lora_train_confidence_token_embedding())
+    lora_train_confidence_token_embedding: bool = Field(
+        default=get_lora_train_confidence_token_embedding()
+    )
     system_message: str = Field(
         default="Reason step by step, then provide the final answer within the last \\boxed{} command of your output."
     )
