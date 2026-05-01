87a14cbba30b2fe02a4113f24479f8f11c48c8d5
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2026-05-01 18:21:21 +0200

    feat: added an evaluate call

--- rl_for_llms/evaluate.py
+++ rl_for_llms/evaluate.py
@@ -1,3 +1,4 @@
+from rl_for_llms.models.method import Method
 from rl_for_llms.models.variant import Variant
 from rl_for_llms.utils.chart_utils import (
     create_answer_accuracy_chart,
@@ -6,12 +7,18 @@ from rl_for_llms.utils.chart_utils import (
 from rl_for_llms.utils.evaluation_utils import (
     get_response_and_confidence_tokens_for_answers,
 )
+from rl_for_llms.utils.inference_time_evaluation_utils import inference_time_evaluate
 
 
 def main() -> None:
     """Execute the main function of the module."""
     create_answer_accuracy_chart()
     create_confidence_chart()
+    inference_time_evaluate(
+        Variant.WITH_CONFREW,
+        Method.DENSE,
+        use_tempmod=True,
+    )
     _ = get_response_and_confidence_tokens_for_answers(Variant.BASE, sample_size=64)
 
 
