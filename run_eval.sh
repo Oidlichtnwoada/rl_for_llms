ad45390bced0b96ef55c9610f4a8c9eba92d2b96
Hannes Brantner <42490916+Oidlichtnwoada@users.noreply.github.com>
2026-05-04 23:50:00 +0200

    feat: add a run eval script

---
+++ run_eval.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+# make sure the output is unbuffered
+export PYTHONUNBUFFERED=1
+
+# make sure experimental warnings from trl are silenced
+export TRL_EXPERIMENTAL_SILENCE=1
+
+# set the correct parameters based on GPU availability
+if command -v nvidia-smi &> /dev/null && nvidia-smi &> /dev/null; then
+    DYNAMO_BACKEND="no"
+    MIXED_PRECISION="no"
+else
+    DYNAMO_BACKEND="no"
+    MIXED_PRECISION="no"
+fi
+
+# run the project (output is visible in terminal and appended to a log file)
+uv run accelerate launch --num_processes=0 --num_machines=1 --mixed_precision="$MIXED_PRECISION" --dynamo_backend="$DYNAMO_BACKEND" -m rl_for_llms.evaluate 2>&1 | tee -a logs/rl_for_llms.evaluate.log
